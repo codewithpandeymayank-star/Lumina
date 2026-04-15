c491e3cb6b8fdcaa9fb25ee91e7393844842b19e
codewithpandeymayank-star <codewithpandeymayank@gmail.com>
2026-04-15 05:47:45 +0530

    fixed landing page

--- app/app.py
+++ app/app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title("Chat")
+st.set_page_config(page_title="Lumina", layout="wide")
+
+# Landing Page UI
+st.title("🧠 Lumina")
+
+st.write("Welcome to Lumina AI Emotion Chatbot")
+
+if st.button("Start Chat"):
+    st.switch_page("pages/Chat.py")
